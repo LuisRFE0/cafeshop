a47a1018f5fdd4dc59a0e53ec43e55eef0c7e717
Luis flores <luis.r.f.e@hotmail.com>
2024-06-18 18:46:20 -0600

    Inicio del sistema cafeShop , con navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import '../src/style/app.css'
+import { Header } from './layout/Header'
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <h1 className='nombreTitulo'>COFFE SHOP</h1>
+        </>)
+
+
+}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,33 @@
+import '../style/header.css'
+
+export const Header = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">CoffeShop</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pedidos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Inventario</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contabilidad</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
