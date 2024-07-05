52c515e3cd0036bae535fa0cca7959d46f818537
Luis flores <luis.r.f.e@hotmail.com>
2024-07-04 19:18:09 -0600

    creacion del menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import { InicioView } from './components/InicioView'
 import { Header } from './layout/Header'
 import { Administradores } from './data/Administradores'
 import { Footer } from './layout/Footer'
+import { PedidosInicio } from './components/PedidosInicio'
 
 export const App = () => {
     const admin = Administradores();
-         
+
 
 
     return (
         <>
             <Header />
-            <InicioView  admin={admin} />
+            <InicioView admin={admin} />
+            <PedidosInicio />
             <Footer />
         </>)
 
